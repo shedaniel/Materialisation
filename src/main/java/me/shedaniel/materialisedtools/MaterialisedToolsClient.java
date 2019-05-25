3cd9ce34d8b0d6ca6372365df00540cc57215c54
Unknown <shekwancheung0528@gmail.com>
2019-05-25 19:09:20 +0800

    Didn't know there is an api for that mixin

--- src/main/java/me/shedaniel/materialisedtools/MaterialisedToolsClient.java
+++ src/main/java/me/shedaniel/materialisedtools/MaterialisedToolsClient.java
@@ -2,8 +2,10 @@ package me.shedaniel.materialisedtools;
 
 import me.shedaniel.materialisedtools.containers.MaterialisingTableContainer;
 import me.shedaniel.materialisedtools.containers.MaterialisingTableScreen;
+import me.shedaniel.materialisedtools.items.MaterialisedToolUtils;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
+import net.fabricmc.fabric.impl.client.render.ColorProviderRegistryImpl;
 import net.minecraft.network.chat.TranslatableComponent;
 
 public class MaterialisedToolsClient implements ClientModInitializer {
@@ -12,5 +14,6 @@ public class MaterialisedToolsClient implements ClientModInitializer {
         ScreenProviderRegistry.INSTANCE.registerFactory(MaterialisedTools.MATERIALISING_TABLE_CONTAINER, (syncId, identifier, playerEntity, packetByteBuf) -> {
             return new MaterialisingTableScreen(new MaterialisingTableContainer(syncId, playerEntity.inventory), playerEntity.inventory, new TranslatableComponent("container.materialisedtools.materialising_table"));
         });
+        ColorProviderRegistryImpl.ITEM.register(MaterialisedToolUtils::getColor, MaterialisedTools.MATERIALISED_PICKAXE);
     }
 }
