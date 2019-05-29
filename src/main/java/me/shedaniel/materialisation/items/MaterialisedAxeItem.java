1671f2de46e02511778cd8545103be9fa1f6de3b
Unknown <shekwancheung0528@gmail.com>
2019-05-29 21:15:23 +0800

    strip wood

--- src/main/java/me/shedaniel/materialisation/items/MaterialisedAxeItem.java
+++ src/main/java/me/shedaniel/materialisation/items/MaterialisedAxeItem.java
@@ -4,20 +4,21 @@ import com.google.common.collect.Sets;
 import me.shedaniel.materialisation.MaterialisationUtils;
 import me.shedaniel.materialisation.ModReference;
 import me.shedaniel.materialisation.mixin.MiningToolItemAccessor;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -25,12 +26,12 @@ import net.minecraft.world.World;
 import java.util.List;
 import java.util.Set;
 
-public class MaterialisedAxeItem extends MiningToolItem implements MaterialisedMiningTool {
+public class MaterialisedAxeItem extends AxeItem implements MaterialisedMiningTool {
     
     private static final Set<Block> EFFECTIVE_BLOCKS = Sets.newHashSet(new Block[]{Blocks.OAK_PLANKS, Blocks.SPRUCE_PLANKS, Blocks.BIRCH_PLANKS, Blocks.JUNGLE_PLANKS, Blocks.ACACIA_PLANKS, Blocks.DARK_OAK_PLANKS, Blocks.BOOKSHELF, Blocks.OAK_WOOD, Blocks.SPRUCE_WOOD, Blocks.BIRCH_WOOD, Blocks.JUNGLE_WOOD, Blocks.ACACIA_WOOD, Blocks.DARK_OAK_WOOD, Blocks.OAK_LOG, Blocks.SPRUCE_LOG, Blocks.BIRCH_LOG, Blocks.JUNGLE_LOG, Blocks.ACACIA_LOG, Blocks.DARK_OAK_LOG, Blocks.CHEST, Blocks.PUMPKIN, Blocks.CARVED_PUMPKIN, Blocks.JACK_O_LANTERN, Blocks.MELON, Blocks.LADDER, Blocks.SCAFFOLDING, Blocks.OAK_BUTTON, Blocks.SPRUCE_BUTTON, Blocks.BIRCH_BUTTON, Blocks.JUNGLE_BUTTON, Blocks.DARK_OAK_BUTTON, Blocks.ACACIA_BUTTON, Blocks.OAK_PRESSURE_PLATE, Blocks.SPRUCE_PRESSURE_PLATE, Blocks.BIRCH_PRESSURE_PLATE, Blocks.JUNGLE_PRESSURE_PLATE, Blocks.DARK_OAK_PRESSURE_PLATE, Blocks.ACACIA_PRESSURE_PLATE});
     
     public MaterialisedAxeItem(Settings settings) {
-        super(0, -3.1F, MaterialisationUtils.DUMMY_MATERIAL, EFFECTIVE_BLOCKS, settings.durability(0));
+        super(MaterialisationUtils.DUMMY_MATERIAL, 0, -3.1F, settings.durability(0));
         addProperty(new Identifier(ModReference.MOD_ID, "handle_isbright"), (itemStack, world, livingEntity) -> {
             return itemStack.getOrCreateTag().containsKey("mt_handle_bright") ? 1f : 0f;
         });
@@ -54,6 +55,34 @@ public class MaterialisedAxeItem extends MiningToolItem implements MaterialisedM
     @Override
     public boolean canEffectivelyBreak(ItemStack itemStack, BlockState state) {
         return EFFECTIVE_BLOCKS.contains(state.getBlock());
+    }
+    
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        ItemStack itemStack = context.getItemStack();
+        BlockState blockState = world.getBlockState(blockPos);
+        Block block = (Block) BLOCK_TRANSFORMATIONS_MAP.get(blockState.getBlock());
+        if (MaterialisationUtils.getToolDurability(itemStack) > 0 && block != null) {
+            PlayerEntity playerEntity_1 = context.getPlayer();
+            world.playSound(playerEntity_1, blockPos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            if (!world.isClient) {
+                world.setBlockState(blockPos, (BlockState) block.getDefaultState().with(PillarBlock.AXIS, blockState.get(PillarBlock.AXIS)), 11);
+                if (playerEntity_1 != null) {
+                    if (!playerEntity_1.world.isClient && (!(playerEntity_1 instanceof PlayerEntity) || !(playerEntity_1.abilities.creativeMode)))
+                        if (MaterialisationUtils.applyDamage(itemStack, 1, playerEntity_1.getRand())) {
+                            playerEntity_1.sendToolBreakStatus(context.getHand());
+                            Item item_1 = itemStack.getItem();
+                            itemStack.subtractAmount(1);
+                            playerEntity_1.incrementStat(Stats.BROKEN.getOrCreateStat(item_1));
+                            MaterialisationUtils.setToolDurability(itemStack, 0);
+                        }
+                }
+            }
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.PASS;
     }
     
     @Override
