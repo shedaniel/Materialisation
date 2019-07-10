a20c122bb2f782ce8a1f063bcf9627bef4c8b1df
Unknown <shekwancheung0528@gmail.com>
2019-07-11 03:55:51 +0800

    whatever say hi to full optifine support

--- src/main/java/me/shedaniel/materialisation/MaterialisationMixinPlugin.java
+++ src/main/java/me/shedaniel/materialisation/MaterialisationMixinPlugin.java
@@ -2,7 +2,8 @@ package me.shedaniel.materialisation;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
-import org.spongepowered.asm.lib.tree.ClassNode;
+import org.spongepowered.asm.lib.Opcodes;
+import org.spongepowered.asm.lib.tree.*;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -34,17 +35,37 @@ public class MaterialisationMixinPlugin implements IMixinConfigPlugin {
     public List<String> getMixins() {
         Mixins.addConfiguration("materialisation.mixins.json");
         if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
-        if (FabricLoader.getInstance().isModLoaded("optifabric")) {
-            System.out.println("[Materialisation] oh fuck...");
-            Mixins.addConfiguration("materialisation.yesoptifine.mixins.json");
-        } else {
-            Mixins.addConfiguration("materialisation.nooptifine.mixins.json");
-        }
-        return Collections.emptyList();
+            if (FabricLoader.getInstance().isModLoaded("optifabric")) {
+                System.out.println("[Materialisation] oh f**k it's time to support optifine...");
+                return Collections.singletonList("FakeItemRendererMixin");
+            } else {
+                Mixins.addConfiguration("materialisation.nooptifine.mixins.json");
+            }
+        return null;
     }
     
     @Override
     public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT || FabricLoader.getInstance().isModLoaded("optifabric")) {
+            if (targetClassName.equals("net.minecraft.client.render.item.ItemRenderer") || targetClassName.equals("net.minecraft.class_918"))
+                for(MethodNode method : targetClass.methods) {
+                    if ((method.name.equals("renderGuiItemOverlay") || method.name.equals("method_4022")) && method.desc.contains("String")) {
+                        InsnList instructions = method.instructions;
+                        AbstractInsnNode first = instructions.get(0);
+                        instructions.insertBefore(first, new VarInsnNode(Opcodes.ALOAD, 1)); // TextRenderer
+                        instructions.insertBefore(first, new VarInsnNode(Opcodes.ALOAD, 2)); // ItemStack
+                        instructions.insertBefore(first, new VarInsnNode(Opcodes.ILOAD, 3)); // x
+                        instructions.insertBefore(first, new VarInsnNode(Opcodes.ILOAD, 4)); // y
+                        MethodInsnNode render;
+                        if (targetClassName.equals("net.minecraft.class_918"))
+                            render = new MethodInsnNode(Opcodes.INVOKESTATIC, "me/shedaniel/materialisation/optifine/RealItemRenderer", "renderGuiItemOverlay", "(Lnet/minecraft/class_1799;II)V", false);
+                        else
+                            render = new MethodInsnNode(Opcodes.INVOKESTATIC, "me/shedaniel/materialisation/optifine/RealItemRenderer", "renderGuiItemOverlay", "(Lnet/minecraft/item/ItemStack;II)V", false);
+                        instructions.insertBefore(first, render);
+                        break;
+                    }
+                }
+        }
     }
     
     @Override
---
+++ src/main/java/me/shedaniel/materialisation/optifine/mixin/FakeItemRendererMixin.java
@@ -0,0 +1,7 @@
+package me.shedaniel.materialisation.optifine.mixin;
+
+import net.minecraft.client.render.item.ItemRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemRenderer.class)
+public class FakeItemRendererMixin {}
